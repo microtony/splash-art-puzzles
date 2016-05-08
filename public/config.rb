0a1ebc8a2623a1b9bae3e7d856a73522a1faef56
Tony Wong <microtony@gmail.com>
2016-05-08 10:06:41 +0800

    Minify css

---
+++ public/config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "stylesheets" # by Compass.app 
+sass_dir = "sass" # by Compass.app 
+images_dir = "images" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = false # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = true # by Compass.app 
